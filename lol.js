48ed56b8b5eaa9b88b58235b8bc9d33b7931b548
asuperhandsomemen <zhuqq10086@gmail.com>
2022-04-20 09:48:11 -0400

    123

---
+++ lol.js
@@ -0,0 +1,46 @@
+function mode1(){
+	document.getElementById("game modes").innerHTML = "Summoner's Rift is the most popular map in League of Legends. In this map mode, two teams each have five players, and they fight against each other with the goal of destroying the other team's main base, the Crystal Nexus. In addition to attacking each other, they also have to defend their own Crystal Nexus from being destroyed. The location of the crystal hub is in the lower left and upper right corners of the map. These two buildings will continuously create low-intensity non-player character minions. The opponent's unit contacts. Players must control the hero, advance with their own team's minions into the opposing team's base, and destroy the opposing team's main base to win the game. Between the roads is a neutral area called the wild area, and inside there are non-player characters that do not belong to both players, called -- wild monsters. In addition, there is a river extending from the upper left to the lower right. Although it is called a river, it does not hinder the movement. Heroes can walk on it freely, which is the same as walking in other places on the map, but it will interact with some runes or hero skills. . In addition, the element canyon is that when the second flying dragon of the game is killed, the summoner canyon will acquire the attributes of the flying dragon that will be reborn, and the canyon will change drastically.";
+	document.getElementById("zzz").src="https://upload.wikimedia.org/wikipedia/en/c/ca/League_of_Legends_Screenshot_2018.png";
+	document.getElementById("game modes").style.fontSize = "20px";
+	document.getElementById("game modes").style.margin = "5px";
+}
+function mode2(){
+	document.getElementById("game modes").innerHTML = "ARAM means All Random, All Mid,that is a five-versus-five mode like Summoner's Rift, but on a map called Howling Abyss with only one long lane, no jungle area, and with champions randomly chosen for players. Given the small size of the map, players must be vigilant in avoiding enemy abilities.";
+	document.getElementById("zzz").src="https://static.wikia.nocookie.net/leagueoflegends/images/0/07/Howling_Abyss_Minimap.png/revision/latest/scale-to-width-down/512?cb=20170222210644";
+	document.getElementById("game modes").style.fontSize = "20px";
+	document.getElementById("game modes").style.margin = "5px";
+}
+function mode3(){
+	document.getElementById("game modes").innerHTML = "Teamfight Tactics is an auto battler released in June 2019 and made a permanent game mode the following month. As with others in its genre, players build a team and battle to be the last one standing. Players do not directly affect combat but position their units on a board for them to fight automatically against opponents each round.";
+	document.getElementById("zzz").src="https://cdn2.unrealengine.com/egs-teamfighttactics-riotgames-g1a-07-1920x1080-370934ba1723.jpg?h=720&resize=1&w=1280";
+	document.getElementById("game modes").style.fontSize = "20px";
+	document.getElementById("game modes").style.margin = "5px";
+}
+function wr(){
+		document.getElementById('tft').innerHTML = "As the most anticipated derivative product of League of Legends players, the League of Legends mobile game officially debuted in the global 10th anniversary live broadcast and launched the mobile version of the League of Legends for reservations. In just half a day, 3.842 million players poured into the official website to make reservations."
+		document.getElementById("tft").style.fontSize = "20px";
+		document.getElementById("tft").style.margin = "5px";
+		document.getElementById("qqq").src="https://cdn1.dotesports.com/wp-content/uploads/2020/10/27091635/ElTI-Q0UcAA3EFg.jpg"
+		document.getElementById('subtitle4').innerHTML = "Wild Rift"
+}
+function lor(){
+		document.getElementById('tft').innerHTML = "Legends of Runeterra (LoR) is a 2020 digital collectible card game developed and published by Riot Games. Inspired by the physical collectible card game Magic: The Gathering, the developers sought to create a game within the same genre that significantly lowered the barrier to entry. Since its release in April 2020, the game has been free-to-play, and is monetised through purchasable cosmetics. The game is available for Microsoft Windows and mobile operating systems iOS and Android."
+		document.getElementById("tft").style.fontSize = "20px";
+		document.getElementById("tft").style.margin = "5px";
+		document.getElementById("qqq").src="https://d.newsweek.com/en/full/1536906/legends-runeterra-card-game-legends.jpg?w=1600&h=900&l=50&t=31&q=88&f=d0c921832b528eb0a95fe3f6c3d4089d"
+		document.getElementById('subtitle4').innerHTML = "Legends of Runeterra";
+}
+function rk(){
+	document.getElementById('tft').innerHTML = "Ruined King: A League of Legends Story is a 2021 turn-based role-playing video game developed by Airship Syndicate and published by Riot Forge. A spin-off to Riot Games' League of Legends, Ruined King uses characters and a setting originating in its parent title.";
+	document.getElementById("tft").style.fontSize = "20px";
+	document.getElementById("tft").style.margin = "5px";
+	document.getElementById("qqq").src="https://images.gog-statics.com/b008f343952c76da66987e9d7f13df4b1a7b7f4654c91475c56feb6cd8796aa1.jpg"
+	document.getElementById('subtitle4').innerHTML = "Ruined King: A League of Legends Story"
+}
+function ar(){
+	document.getElementById('tft').innerHTML = "Arcane (titled onscreen as Arcane: League of Legends) is a computer-animated action-adventure streaming television series created by Christian Linke and Alex Yee for Netflix. Produced by Fortiche under the supervision of Riot Games, it is set in the League of Legends fictional universe, and primarily focuses on sisters Vi and Jinx. The series was announced at the League of Legends 10th anniversary celebration in 2019, and first released in November 2021."
+	document.getElementById("tft").style.fontSize = "20px";
+	document.getElementById("tft").style.margin = "5px";
+	document.getElementById('qqq').src="https://upload.wikimedia.org/wikipedia/en/c/cd/Arcane_intertitle.png"
+	document.getElementById('subtitle4').innerHTML = "Arcane"
+}
